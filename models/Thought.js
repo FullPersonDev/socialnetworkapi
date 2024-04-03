889f7feaafab236cbd66ac1d4fc3585ab5fe91a3
Martin Aguero <martin.aguero84@gmail.com>
2024-04-02 21:15:18 -0500

    added the reactionSchema to the Thought model and ensured it was embeded as an array of subdocuments within the Thought model. Finish off the models by adding the index.js to export both User and Thought models.

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,20 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+
+//Define the Reaction schema
+const reactionSchema = new Schema(
+    {
+        reactionId: {type: Schema.Types.ObjectId, default: () => new Types.ObjectId()},
+        reactionBody: {type: String, required: true, maxlength: 280},
+        username: {type: Schema.Types.ObjectId, ref: 'user'},
+        createdAt: {type: Date, default: Date.now, get: (createdAtVal) => createdAtVal.toLocalString()},
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
 
 //Schema that makes up a throught
 const thoughtSchema = new Schema(
@@ -6,7 +22,8 @@ const thoughtSchema = new Schema(
         thoughtText: {type: String, required: true, minlength: 1, maxlength: 280},
         createdAt: {type: Date, default: Date.now, get: (createdAtVal) => createdAtVal.toLocalString()},
         username: {type: Schema.Types.ObjectId, ref: 'user'},
-        reactions: [{type: Schema.Types.ObjectId, ref: 'reaction'}],
+        //Embed reactionSchema as an array of subdocuments
+        reactions: [reactionSchema],
     },
     {
         toJSON: {
